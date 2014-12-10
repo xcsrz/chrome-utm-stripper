d5f1791a83c06d41895e6b6a5fdb801fa9b2d090
xcsrz <xcsrz@excessorize.us>
2014-12-10 13:32:47 -0800

    Update background.js
    switch from updating the url after the request to changing the url before the request.

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    var queryStringIndex = tab.url.indexOf('?');
-    if (tab.url.indexOf('utm_') > queryStringIndex) {
-        var stripped = tab.url.replace(
-            /([\?\&]utm_(source|medium|term|campaign|content|cid|reader)=[^&#]+)/ig,
-            '');
-        if (stripped.charAt(queryStringIndex) === '&') {
-            stripped = stripped.substr(0, queryStringIndex) + '?' +
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var queryStringIndex = details.url.indexOf('?');
+        if (details.url.indexOf('utm_') > queryStringIndex) {
+            var stripped = details.url.replace(
+                /([\?\&]utm_(source|medium|term|campaign|content|cid|reader)=[^&#]+)/ig,
+                '');
+            if (stripped.charAt(queryStringIndex) === '&') {
+                stripped = stripped.substr(0, queryStringIndex) + '?' +
                 stripped.substr(queryStringIndex + 1)
+            }
+            return { url: stripped };
         }
-        if (stripped != tab.url) {
-            chrome.tabs.update(tab.id, {url: stripped});
-        }
-    }
-});
+        return {};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking"]
+);
